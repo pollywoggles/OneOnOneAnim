0477220c432579d88438a93072ccfaf3022c63c9
pollywoggles <pollywoggles@yahoo.com>
2015-10-23 23:24:03 -0700

    Update home.js

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,1 +1,18 @@
+define(["knockout", "text!./home.html"],
 
+    function (ko, view) {
+
+    "use strict";
+
+    function viewModel(params) {
+
+        var self = this;
+
+
+
+        return self;
+
+    }
+
+    return { viewModel: viewModel, template: view };
+});
