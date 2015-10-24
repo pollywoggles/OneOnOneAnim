08edd5908fa4753d9becd521590a7961331399e0
pollywoggles <pollywoggles@yahoo.com>
2015-10-23 23:38:39 -0700

    Update require.config.js

--- app/boot/require.config.js
+++ app/boot/require.config.js
@@ -1,15 +1,22 @@
-﻿var require = {
+var require = {
     baseUrl: "/",
     paths: {
-        "bootstrap": "OneOnOneAnim/scripts/libs/bootstrap/bootstrap",
-        "crossroads": "OneOnOneAnim/scripts/libs/crossroads/crossroads",
+
+        //general
         "jquery": "OneOnOneAnim/scripts/libs/jquery/jquery-1.9.0",
         "knockout": "OneOnOneAnim/scripts/libs/knockout/knockout-3.2.0beta.debug",
         "knockout-projections": "OneOnOneAnim/scripts/libs/knockout/knockout-projections.min",
+
+        //layout
+        "bootstrap": "OneOnOneAnim/scripts/libs/bootstrap/bootstrap",
+
+        //routing
+        "crossroads": "OneOnOneAnim/scripts/libs/crossroads/crossroads",
         "signals": "OneOnOneAnim/scripts/libs/crossroads/signals",
         "hasher": "OneOnOneAnim/scripts/libs/crossroads/hasher",
         "text": "OneOnOneAnim/scripts/libs/require/text",
         "router": "OneOnOneAnim/app/boot/router"
+
     },
     shim: {
         "bootstrap": {
