6f772995df057b6ff23079c9fb68697416f0e84c
pollywoggles <pollywoggles@yahoo.com>
2015-10-24 09:14:28 -0700

    Create pages.js

---
+++ app/pages/pages.js
@@ -0,0 +1,10 @@
+define(function() {
+
+    return [
+
+        {name: "home", path: "OneOnOneAnim/app/pages/home/home", url: ""},
+        {name: "about", path: "OneOnOneAnim/app/pages/about/about", url: "about"}
+
+    ]
+
+});
